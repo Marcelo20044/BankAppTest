0dd0c59d58b3416f7ce83a216285f41a1d445392
Khvostenko <khmark04@mail.ru>
2021-10-01 17:37:42 +0700

    initial commit

---
+++ src/main.java
@@ -0,0 +1,10 @@
+public class main {
+
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int moneyTransfer = 500_000_000;
+        int total = balance + moneyTransfer;
+        System.out.println(total);
+
+    }    }
+
